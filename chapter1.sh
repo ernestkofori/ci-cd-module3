e441bd1c193a3944554635f1f1a5e3ce636339cc
Ernest Ofori <ernextk@gmail.com>
2025-10-30 18:10:25 +0000

    addinging before merge 2

--- chapter1.sh
+++ chapter1.sh
@@ -5,3 +5,6 @@ echo "This is a message I added to test how the stash works. I needed to add thi
 echo "adding this to test stash"
 echo "added to practice git revert"
 echo "this is what I am adding to the main before merging"
+echo "This is a message I added to test how the stash works. I needed to add this cos I add already"
+echo "adding this to test stash"
+echo "added to practice git revert"echo "This is another test I am adding for the purpose of going back and forth within the history"
