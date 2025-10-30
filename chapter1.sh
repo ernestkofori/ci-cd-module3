32ebe61de619f915cfe6c6c8d9eee23ebbba1d5f
Ernest Ofori <ernextk@gmail.com>
2025-10-30 18:16:41 +0000

    adding before merge from main

--- chapter1.sh
+++ chapter1.sh
@@ -3,8 +3,13 @@ echo "Hello world. This is me learning git as part of my CI/CD journey"
 echo "I just added a second text"
 echo "This is a message I added to test how the stash works. I needed to add this cos I add already"
 echo "adding this to test stash"
+<<<<<<< HEAD
 echo "added to practice git revert"
 echo "this is what I am adding to the main before merging"
 echo "This is a message I added to test how the stash works. I needed to add this cos I add already"
 echo "adding this to test stash"
 echo "added to practice git revert"echo "This is another test I am adding for the purpose of going back and forth within the history"
+=======
+echo "added to practice git revert"echo "This is another test I am adding for the purpose of going back and forth within the history"
+echo "This is another line added to practice git merge from main branch"
+>>>>>>> learning/mod1
