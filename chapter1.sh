4efeecf9cdb32659db057d85da9a544a73b5b477
Ernest Ofori <ernextk@gmail.com>
2025-10-30 10:28:13 +0000

    echo line added for practice stash

--- chapter1.sh
+++ chapter1.sh
@@ -3,3 +3,4 @@
 
 echo "Hello world. This is me learning git as part of my CI/CD journey"
 echo "I just added a second text"
+echo "This is another test I am adding for the purpose of going back and forth within the history"
