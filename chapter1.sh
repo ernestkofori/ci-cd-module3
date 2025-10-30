f3da6dbf8c99550fe950aa151a9af1951aa7fade
Ernest Ofori <ernextk@gmail.com>
2025-10-30 18:21:14 +0000

    commit after merge

--- chapter1.sh
+++ chapter1.sh
@@ -6,13 +6,14 @@ echo "adding this to test stash"
 echo "added to practice git revert"echo "This is another test I am adding for the purpose of going back and forth within the history"
 echo "This is another line added to practice git merge from main branch"echo "This is a message I added to test how the stash works. I needed to add this cos I add already"
 echo "adding this to test stash"
-<<<<<<< HEAD
+
 echo "added to practice git revert"
 echo "this is what I am adding to the main before merging"
 echo "This is a message I added to test how the stash works. I needed to add this cos I add already"
 echo "adding this to test stash"
 echo "added to practice git revert"echo "This is another test I am adding for the purpose of going back and forth within the history"
-=======
-echo "added to practice git revert"echo "This is another test I am adding for the purpose of going back and forth within the history"
+
+echo "added to practice git revert"
+echo "This is another test I am adding for the purpose of going back and forth within the history"
 echo "This is another line added to practice git merge from main branch"
->>>>>>> learning/mod1
+
