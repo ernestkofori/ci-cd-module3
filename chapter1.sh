a2a3fe2498dfab70a99ef3dee3e9d8dbc027c668
Ernest Ofori <ernextk@gmail.com>
2025-10-30 10:06:35 +0000

    Added this supposed to be a stash test but didn't work

--- chapter1.sh
+++ chapter1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
-
 echo "Hello world. This is me learning git as part of my CI/CD journey"
 echo "I just added a second text"
+echo "This is a message I added to test how the stash works. I needed to add this cos I add already"
